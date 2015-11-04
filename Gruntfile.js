e0ac6847bf7df24d739035cb13180fa0f09d7630
Vincent de Belleval <2000db@gmail.com>
2015-11-04 22:05:47 +0000

    fixed path

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,8 +29,8 @@ module.exports = function(grunt) {
                 overwrite: true,
                 replacements: [{
                     from: 'http://localhost/~vdb/evocal-web/src/',
-                    to: 'http://localhost/~vdb/evocal-web/public/'
-                    //to: 'http://evoc.al/'
+                    //to: 'http://localhost/~vdb/evocal-web/public/'
+                    to: 'http://evoc.al/'
                 }, {
                     from: '<analytics></analytics>',
                     to: "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-69336628-1', 'auto');ga('send', 'pageview');</script>"
